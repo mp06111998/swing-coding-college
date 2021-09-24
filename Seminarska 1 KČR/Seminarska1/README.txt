e368a193d6bbda665d57f7b265d32626b006223f
mp06111998 <90275511+mp06111998@users.noreply.github.com>
2021-09-24 10:19:40 +0200

    Add files via upload

---
+++ Seminarska 1 KČR/Seminarska1/README.txt
@@ -0,0 +1,13 @@
+Podatki za AVTENTIKACIJO:
+
+	Ime= Janko
+    Priimek= Metek
+      Geslo= 1337h4x0r
+
+
+Še opomba: Pri oknih avtentikacija, plačilo UPN in nakazilo na isto poslovalnico.
+	   Se zgoraj pod Meni pojavi tudi gumb Domov, ki nas lahko vrne na prejšno okno.
+	   Ob kliku na možnost Odjava pod Meni se nam izpiše zgodovina opravljenih aktivnosti,
+	   če pa zapremo okno z križcem, pa se nam to obvestilo ne pojavi.
+
+										Marcel Polanc
---
+++ Seminarska 1 KČR/Seminarska1/src/seminarska1/Seminarska1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seminarska1;
+
+/**
+ *
+ * @author marce
+ */
+public class Seminarska1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Okvir().setVisible(true);
+    }
+    
+}
